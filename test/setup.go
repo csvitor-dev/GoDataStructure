b4f6e4c1d244f77b2105d291dde95f0bfb9af854
csvitor-dev <csvitor.dev@gmail.com>
2024-10-18 17:59:51 -0300

    test(singly linked list): adding first test cases to structure

--- test/setup.go
+++ test/setup.go
@@ -1,8 +1,22 @@
 package test
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
-// message: defines a generic message for failed tests
-func message(details string, expected any, received any) string {
-	return fmt.Sprintf("%v | expected: %v, received: %v", details, expected, received)
+type Input struct {
+	Values []int
+	Index int
+}
+
+type Output struct {
+	Value int
+	Err bool
+}
+
+// ErrorMessage: defines a generic message for failed tests
+func ErrorMessage(t *testing.T, details string, expected any, received any) {
+	message := fmt.Sprintf("%v | expected: %v, received: %v", details, expected, received)
+	t.Error(message)
 }
--- test/singly_linked_list_test.go
+++ test/singly_linked_list_test.go
@@ -5,36 +5,60 @@ import (
 	"testing"
 )
 
-func Test_EmptyList(t *testing.T) {
+func Test_VerifyEmptyList(t *testing.T) {
 	ll := NewSinglyLinkedList[int]()
 	_, err := ll.Delete()
 
 	if err == nil {
-		t.Errorf(message("deleting elements does not behave well", nil, err))
+		ErrorMessage(t, "deleting elements does not behave well", nil, err)
 	}
 	_, err = ll.RemoveAt(3)
 
 	if err == nil {
-		t.Error(message("deleting elements does not behave well", nil, err))
+		ErrorMessage(t, "deleting elements does not behave well", nil, err)
 	}
 }
 
 func Test_FindElements(t *testing.T) {
+	cases := []struct {
+		got Input
+		expected Output
+	}{
+		{
+			Input{ []int{ 2, 7, 5 }, 0, },
+			Output{ 2, false, },
+		},
+		{
+			Input{ []int{ 2, 10, 8, 19 }, 2, },
+			Output{ 8, false, },
+		},
+		{
+			Input{ []int{}, 9, },
+			Output{ 0, true, },
+		},
+	}
 
+	for _, tc := range cases {
+		ll := NewSinglyLinkedList[int]()
+
+		for _, v := range tc.got.Values {
+			ll.Add(v)
+		}
+
+		if v, err := FindOnIndex(ll, tc.got.Index); 
+			(err != nil) != tc.expected.Err || v != tc.expected.Value {
+			ErrorMessage(t, "search for element on index failed", tc.expected.Value, v)
+		}
+	}
 }
 
-func Test_AddElements(t *testing.T) {
-	
+func Test_AddTwoElements(t *testing.T) {
 	ll := NewSinglyLinkedList[int]()
 	ll.Add(3)
 	ll.Add(5)
-
-	if ll.Length() != 2 {
-		t.Error(message("adding elements misbehaves", 2, ll.Length()))
-	}
-
-	if v, err := FindOnIndex(ll, 1); err != nil || v != 5 {
-		t.Error(message("constant insertion fail", 5, v))
+	
+	if length := ll.Length(); length != 2 {
+		ErrorMessage(t, "adding elements misbehaves", 2, length)
 	}
 }
 
